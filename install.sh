672e281da39634afe8d3a76bf1cb3eac9dd023a5
Andreas Tennert <mail@andreas-tennert.de>
2020-11-16 12:00:20 +0100

    build: add install script

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo ""
+echo "=========================================="
+echo "lcarsde application starter installation"
+echo "=========================================="
+echo ""
+echo "This program requires:"
+echo "* Python 3.8"
+echo "* Python 3 PyGObject"
+echo "* Python posix-ipc"
+echo ""
+
+cp ./src/lcarsde-app-menu.py /usr/bin/lcarsde-app-menu.py
